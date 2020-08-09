7da61f60310eb73880c742aa69a6b28cf5f6309d
Kirtana Madiraju <kmadiraju19@gmail.com>
2020-08-09 13:07:23 -0400

    Updates

---
+++ precache-manifest.b1c1188d9a7960fc60b838eec9bbe2f6.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ac00ba7450e408ec1bad7cb44ab52ba",
+    "url": "/hw-20-reactPortfolio/index.html"
+  },
+  {
+    "revision": "ba254fc4887cdc7f0397",
+    "url": "/hw-20-reactPortfolio/static/css/main.a90eeae0.chunk.css"
+  },
+  {
+    "revision": "91ff3438e69951c76c13",
+    "url": "/hw-20-reactPortfolio/static/js/2.b1ff539b.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/hw-20-reactPortfolio/static/js/2.b1ff539b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba254fc4887cdc7f0397",
+    "url": "/hw-20-reactPortfolio/static/js/main.ed3b678d.chunk.js"
+  },
+  {
+    "revision": "f1fd0b24be4d5a64b560",
+    "url": "/hw-20-reactPortfolio/static/js/runtime-main.f82ef35b.js"
+  },
+  {
+    "revision": "3dc811a373bb2083860f95beb1bdd28f",
+    "url": "/hw-20-reactPortfolio/static/media/daily.3dc811a3.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hw-20-reactPortfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "6922210ff8ff94bb05769a071c1830a1",
+    "url": "/hw-20-reactPortfolio/static/media/quiz.6922210f.png"
+  },
+  {
+    "revision": "32e8055c2ee1b28f2272a13fc3e01e43",
+    "url": "/hw-20-reactPortfolio/static/media/readMe.32e8055c.jpeg"
+  },
+  {
+    "revision": "c3f4227260de6046aa673648c70b78cc",
+    "url": "/hw-20-reactPortfolio/static/media/sun.c3f42272.png"
+  },
+  {
+    "revision": "a70a1fa8643feaf7a03381c22dfe4c45",
+    "url": "/hw-20-reactPortfolio/static/media/weather.a70a1fa8.png"
+  }
+]);
